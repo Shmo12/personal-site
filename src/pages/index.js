03bbe40286a5fa807163b722981f0afca6a6e22f
Jeff <jeffreyherpers@gmail.com>
2020-09-16 02:34:07 -0500

    prismic update

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,43 +6,37 @@ import { graphql, Link } from "gatsby";
 import styled from "@emotion/styled";
 import colors from "styles/colors";
 import dimensions from "styles/dimensions";
+import Button from "components/_ui/Button";
 import About from "components/About";
 import Layout from "components/Layout";
-import ProjectCard from "components/ProjectCard";
+import BlogCard from "components/BlogCard";
 
 const Hero = styled("div")`
     padding-top: 2.5em;
     padding-bottom: 3em;
     margin-bottom: 6em;
     max-width: 830px;
-
     @media(max-width:${dimensions.maxwidthMobile}px) {
        margin-bottom: 3em;
     }
-
     h1 {
         margin-bottom: 1em;
-
         a {
             text-decoration: none;
             transition: all 100ms ease-in-out;
-
             &:nth-of-type(1) { color: ${colors.blue500}; }
             &:nth-of-type(2) { color: ${colors.orange500}; }
             &:nth-of-type(3) { color: ${colors.purple500}; }
             &:nth-of-type(4) { color: ${colors.green500}; }
             &:nth-of-type(5) { color: ${colors.teal500}; }
-
             &:hover {
                 cursor: pointer;
                 transition: all 100ms ease-in-out;
-
                 &:nth-of-type(1) { color: ${colors.blue600};    background-color: ${colors.blue200};}
                 &:nth-of-type(2) { color: ${colors.orange600};  background-color: ${colors.orange200};}
                 &:nth-of-type(3) { color: ${colors.purple600};  background-color: ${colors.purple200};}
                 &:nth-of-type(4) { color: ${colors.green600};   background-color: ${colors.green200};}
                 &:nth-of-type(5) { color: ${colors.teal600};    background-color: ${colors.teal200};}
-
             }
         }
     }
@@ -52,11 +46,9 @@ const Section = styled("div")`
     margin-bottom: 10em;
     display: flex;
     flex-direction: column;
-
     @media(max-width:${dimensions.maxwidthTablet}px) {
         margin-bottom: 4em;
     }
-
     &:last-of-type {
         margin-bottom: 0;
     }
@@ -68,22 +60,18 @@ const WorkAction = styled(Link)`
     color: currentColor;
     transition: all 150ms ease-in-out;
     margin-left: auto;
-
     @media(max-width:${dimensions.maxwidthTablet}px) {
        margin: 0 auto;
     }
-
     span {
         margin-left: 1em;
         transform: translateX(-8px);
         display: inline-block;
         transition: transform 400ms ease-in-out;
     }
-
     &:hover {
         color: ${colors.blue500};
         transition: all 150ms ease-in-out;
-
         span {
             transform: translateX(0px);
             opacity: 1;
@@ -136,10 +124,16 @@ const RenderBody = ({ home, projects, meta }) => (
             <>
                 {RichText.render(home.hero_title)}
             </>
+            <a href={home.hero_button_link.url}
+               target="_blank" rel="noopener noreferrer">
+                <Button>
+                    {RichText.render(home.hero_button_text)}
+                </Button>
+            </a>
         </Hero>
         <Section>
             {projects.map((project, i) => (
-                <ProjectCard
+                <BlogCard
                     key={i}
                     category={project.node.project_category}
                     title={project.node.project_title}
@@ -190,6 +184,12 @@ export const query = graphql`
                 edges {
                     node {
                         hero_title
+                        hero_button_text
+                        hero_button_link {
+                            ... on PRISMIC__ExternalLink {
+                                _linkType
+                                url
+                            }
                         }
                         content
                         about_title
